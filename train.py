0749e8c36dd5fb43b64e388d1215c4348db48abb
KKMAI <khotchakorn61@gmail.com>
2025-12-03 01:34:12 +0700

    add result.txt

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 # train.py : ตัวอย่าง ML ง่ายที่สุด
+# pip install scikit-learn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
